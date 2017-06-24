a7564850849cf66700cbf01b8fbb4b9c16ac29a6
root <root@localhost.localdomain>
2017-06-24 18:15:11 +0530

    fend

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,43 +1,53 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {SecondPage} from "../second/second";
+import {DatafetchProvider} from "../../providers/datafetch/datafetch";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  username:string;
-  password:string;
-  addcomment:string;
-  student_name:string;
-  batch:string;
-  year:string;
-  comment:any=["abc","adf","bawa"];
-  students:any=[{"name":"xyz","batch":"2013","year":"second"},
-    {"name":"bawa","batch":"2014","year":"third"},
-    {"name":"bawaa","batch":"2014","year":"first"}]
-  constructor(public navCtrl: NavController) {
+  username: string;
+  password: string;
+  addcomment: string;
+  student_name: string;
+  batch: string;
+  year: string;
+  comment: any = ["abc", "adf", "bawa"];
+  students: any;
 
+  constructor(public navCtrl: NavController, public datafetch: DatafetchProvider) {
+    this.getdata();
   }
-  gotosecondpage(){
+
+  gotosecondpage() {
     this.navCtrl.setRoot(SecondPage);
   }
+
   login() {
     if (this.username == "ali" && this.password == "12345678") {
       this.navCtrl.push(SecondPage, {"user": this.username});
     }
   }
-  addcomments(){
+
+  addcomments() {
     this.comment.push(this.addcomment);
-    this.addcomment="";
+    this.addcomment = "";
   }
-  addstudent(){
-    this.students.push({"name":this.student_name,"batch":this.batch,"year":this.year});
+
+  addstudent() {
+    this.students.push({"name": this.student_name, "batch": this.batch, "year": this.year});
 
 
-    this.student_name="";
-    this.year="";
-    this.batch="";
+    this.student_name = "";
+    this.year = "";
+    this.batch = "";
+  }
+
+  getdata() {
+    this.datafetch.load().then((data)=>{
+      console.log(data);
+    })
   }
 }
